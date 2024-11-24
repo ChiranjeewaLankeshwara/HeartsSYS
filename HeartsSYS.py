84c8471371303a6c9ac84029a9c42b4a6f25eb09
Chiranjeewa Peellawatta <chiranlankeshwara2003@gmail.com>
2024-11-24 16:24:45 +0530

    Update HeartsSYS.py

--- HeartsSYS.py
+++ HeartsSYS.py
@@ -8,3 +8,19 @@ def draw_heart(w, h, color, start_pos):
     t.down()
     t.fillcolor(color)
     t.begin_fill()
+
+  a = 0
+    points = []
+    while a < 2 * pi:
+        x = w * (16 * sin(a) ** 3) + start_pos[0]
+        y = h * (13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a)) + start_pos[1]
+        points.append((x, y))
+        if len(points) == 1:
+            t.up()
+            t.goto(points[0])  # Move to the starting point
+            t.down()
+        else:
+            t.goto(x, y)
+        a += 0.01
+    t.goto(points[0])  # Close the shape
+    t.end_fill()
