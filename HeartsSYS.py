f7cbbaf6f4f8720928d134736d10b70d094faefe
Chiranjeewa Peellawatta <chiranlankeshwara2003@gmail.com>
2024-11-24 14:03:22 +0530

    Update HeartsSYS.py

--- HeartsSYS.py
+++ HeartsSYS.py
@@ -4,7 +4,7 @@ from math import pi, sin, cos
 def draw_heart(w, h, color, start_pos):
     t.pensize(2.5)
     t.up()
-    t.goto(start_pos)  # Start at a specified position
+    t.goto(start_pos) 
     t.down()
     t.fillcolor(color)
     t.begin_fill()
